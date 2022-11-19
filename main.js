f57e552f01d11f53f265818a8a11467520245b31
Zalobowski <zalobowskijs@gmail.com>
2022-11-19 19:38:53 +0100

    Added the ui and made the game fully playable

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const btns = document.querySelectorAll('.btn');
+const emojiUiContainers = document.querySelectorAll(".emoji");
 
+const playerScoreUiContainer = document.querySelector('.playerScore');
+const computerScoreUiContainer = document.querySelector('.computerScore');
+const roundResultUiContainer = document.querySelector('.round-result');
+
+let playerScore = 0;
+let computerScore = 0;
 
 let usersSelection;
 let computerSelection
@@ -7,19 +14,34 @@ for(let i = 0; i < btns.length; i++){
     btns[i].addEventListener('click', () => {
         usersSelection = btns[i].getAttribute('id');
         computerSelection = getComputerChoice();
-
-        console.log(usersSelection);
-        console.log(computerSelection)
-
-        console.log(check(usersSelection, computerSelection));
+        updateUi(usersSelection, computerSelection, check(usersSelection, computerSelection))
   })
 } 
+
 
 const emojis = {
     Rock: "✊",
     Paper: "✋",
-    Scissors: "✌️"
+    Scissors: "✌️",
 }
+
+const updateUi = function (playersSelection, computerSelection, gameResult) {
+
+
+    if(gameResult == "Player1 Wins"){
+        playerScore  += 1;
+    } else if(gameResult == "Computer wins"){
+        computerScore  += 1;
+    }
+
+    emojiUiContainers[0].textContent = emojis[playersSelection];
+    emojiUiContainers[1].textContent = emojis[computerSelection];
+    playerScoreUiContainer.textContent = playerScore;
+    computerScoreUiContainer.textContent = computerScore;
+    roundResultUiContainer.textContent = gameResult;
+
+}
+
 
 // this function checks who the winner is it take 2 parameters which is players selection and computer selection (Rock, Paper or Scissors) it checks if
 // the two parameters are the same it return a draw else it checks the winning conditions 
@@ -27,13 +49,13 @@ const check = function (playersSelection, computersSelection) {
     if(playersSelection == computersSelection){
         return "Draw";
     } else if((playersSelection == "Rock"|| computersSelection == "Rock") && (playersSelection == "Scissors"|| computersSelection == "Scissors")){
-        if(playersSelection == "Rock") return "player1 Wins";
+        if(playersSelection == "Rock") return "Player1 Wins";
         else return "Computer wins";
     } else if((playersSelection == "Paper"|| computersSelection == "Paper") && (playersSelection == "Rock" || computersSelection == "Rock")){
-        if(playersSelection == "Paper") return "player1 Wins";
+        if(playersSelection == "Paper") return "Player1 Wins";
         else return "Computer wins";
     } else if((playersSelection == "Scissors" || computersSelection == "Scissors") && (playersSelection == "Paper" || computersSelection == "Paper")){
-        if(playersSelection == "Scissors") return "player1 Wins";
+        if(playersSelection == "Scissors") return "Player1 Wins";
         else return "Computer wins"
     }
 }
